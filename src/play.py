6ca307cd32fcb32f05dcaa6f426c823c3d0422b5
erez <ahar.erez@gmail.com>
2021-10-30 17:08:48 +0300

    generate notebook

--- src/play.py
+++ src/play.py
@@ -3,15 +3,15 @@ import pandas as pd
 a = 10
 
 
-def something():
+def do_something():
     df = pd.DataFrame({'k': [1, 2], 'l': [2, 6]})
-    return something_else(df)
+    return do_something_else(df)
 
 
-def something_else(df):
+def do_something_else(df):
     c = a + 5
     return c
 
 
-d = something()
+d = do_something()
 pass
