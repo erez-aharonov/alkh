7f47083113cc1e3e8a9bf1e9c2b431f2561bcd9d
erez <ahar.erez@gmail.com>
2021-11-05 15:05:45 +0200

    print context inside notebook

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from alkh.src.dump_data import take_it_offline
+from alkh.src.print_utils import print_context
---
+++ src/print_utils.py
@@ -0,0 +1,6 @@
+from typing import *
+
+
+def print_context(context_lines_list: List):
+    for line in context_lines_list:
+        print(line)
