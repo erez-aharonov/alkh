f1683a513b4c1a164acb942bd9ecba0326ecd4c6
erez <ahar.erez@gmail.com>
2022-03-05 06:31:53 +0200

    add imports and variables instantiation

--- alkh/__init__.py
+++ alkh/__init__.py
@@ -2,5 +2,5 @@ from alkh.dump_data import take_it_offline
 from alkh.print_utils import print_context
 
 
-version_info = (0, 0, 1)
+version_info = (0, 0, 1, 1)
 __version__ = '.'.join(map(str, version_info))
