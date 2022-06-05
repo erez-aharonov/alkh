9ca9be67b896c8b5a42990da2fc9f4cd38413b60
erez <ahar.erez@gmail.com>
2022-06-05 16:35:48 +0300

    add test

---
+++ tests/dump_data_test.py
@@ -0,0 +1,18 @@
+import sys
+print(sys.path)
+sys.path.append('/alkh/')
+import alkh
+
+
+def do_something(a: int):
+    k = do_something_2(a)
+    k2 = k + 1
+    alkh.take_it_offline('/alkh/tests')
+    return k2
+
+
+def do_something_2(b: int):
+    return b + 1
+
+
+a_number = do_something(5)
