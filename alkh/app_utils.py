33799b5132936f613c6ade529cf576ee02d43147
erez <ahar.erez@gmail.com>
2022-08-26 14:17:21 +0300

    play with app creation

--- alkh/app_utils.py
+++ alkh/app_utils.py
@@ -1,8 +1,22 @@
 import os
 
 dir_path = os.path.dirname(__file__)
-prism_twilight_min_css_file_path = os.path.join(dir_path, 'assets', 'prism-twilight.min.css')
-prism_twilight_min_css_string = open(prism_twilight_min_css_file_path).read()
+
+
+def _read_asset_file(dir_name, file_name):
+    file_path = os.path.join(dir_path, 'assets', dir_name, file_name)
+    file_string = open(file_path).read()
+    return file_string
+
+
+def _read_script_file(file_name: str):
+    file_string = _read_asset_file('scripts', file_name)
+    return file_string
+
+
+def _read_css_file(file_name: str):
+    file_string = _read_asset_file('css', file_name)
+    return file_string
 
 
 def get_html(lines_numbers_string, file_content):
@@ -10,20 +24,17 @@ def get_html(lines_numbers_string, file_content):
 <!DOCTYPE html>
 <html>
 <head>
-    <style>{prism_twilight_min_css_string}</style>
-    <link href="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/plugins/line-numbers/prism-line-numbers.min.css" 
-    rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/plugins/line-highlight/prism-line-highlight.min.css" 
-    rel="stylesheet" />		
+    <style>{_read_css_file('prism-twilight.min.css')}</style>
+    <style>{_read_css_file('prism-line-highlight.min.css')}</style>
+    <style>{_read_css_file('prism-line-numbers.min.css')}</style>
 </head>
 <body class="line-numbers"> 
     <header data-plugin-header="line-highlight"></header>
     <pre class="language-python" data-line="{lines_numbers_string}"><code>{file_content}</code></pre>
-    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/prism.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/plugins/autoloader/prism-autoloader.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/plugins/line-numbers/prism-line-numbers.min.js"></script>		
-    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/plugins/line-highlight/prism-line-highlight.min.js">
-    </script>
+    <script>{_read_script_file("prism.min.js")}</script>     
+    <script>{_read_script_file("prism-line-numbers.min.js")}</script>
+    <script>{_read_script_file("prism-line-highlight.min.js")}</script>    
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/plugins/autoloader/prism-autoloader.min.js"></script>      
 </body>
 </html>
 """
