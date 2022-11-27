12b0925df836f3927c060efb0dc7d5664adfad30
erez <erez@frontline-pcb.com>
2022-11-27 18:51:37 +0200

    support multi line assignment

--- tests/cst_utils_test.py
+++ tests/cst_utils_test.py
@@ -1,7 +1,6 @@
 from alkh.cst_utils import CallGraphManager
 
 file_path = '/mnt/dev/open_source_projects/alkh/notebooks/play.py'
-# file_path = '/mnt/dev/azure_projects/ATool/core/aoi_defects_clustering/aoi_defect_clustering.py'
 line_number = 6
 call_graph_manager = CallGraphManager(file_path)
 final_lines_numbers_list = call_graph_manager.get_lines_numbers_affecting_line_number(line_number)
