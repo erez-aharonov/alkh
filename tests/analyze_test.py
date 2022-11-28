e51623924c8f1b0260d266e33c85ec8266eaa879
erez <erez@frontline-pcb.com>
2022-11-28 18:38:20 +0200

    update analyze_test.py

--- tests/analyze_test.py
+++ tests/analyze_test.py
@@ -3,18 +3,27 @@ alkh.analyze()
 import pandas as pd
 
 
+jj = 20
+m = 200
+
+
 class A:
     k = 8
+    m = pd.Series({"c": 20})
+    m.c = 50
+    ll = 301
 
     def __init__(self):
-        b = 8 + self.k
-        df = pd.DataFrame({'b': b})
+        self.m.c = 60
+        self.ll = 400
+        self.k, ll = 9, 10 + jj
+        b, mm = \
+            8 + self.k + self.m.c, self.k
         pass
 
-    @staticmethod
-    def run(
-            n)\
+    def run(self, n)\
             -> int:
+        self.m.c = 680
         a = 5
         b = a + 7 + 5.0
         ll = a + 6.4
@@ -35,3 +44,6 @@ class B:
         ll = a + 6.4
         c = a + b + 3
         d = b + c
+
+
+a = A()
