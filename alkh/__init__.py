95c10638b43e67a4cc9959ad9acf9e3cda5449c4
erez <erez@frontline-pcb.com>
2022-11-28 18:48:06 +0200

    move to version 0.3.0

--- alkh/__init__.py
+++ alkh/__init__.py
@@ -3,5 +3,5 @@ from alkh.print_utils import print_context
 from alkh.analyze_call_graph import analyze
 
 
-version_info = (0, 2, 0)
+version_info = (0, 3, 0)
 __version__ = '.'.join(map(str, version_info))
