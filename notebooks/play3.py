5b18f53ddb6c9a439ff1035460daf10325a2359d
erez <erez@kla-tencor.com>
2023-03-01 07:16:31 +0200

    add scopes graph

--- notebooks/play3.py
+++ notebooks/play3.py
@@ -1,6 +1,16 @@
 class A:
     def __init__(self):
         self.a()
+        self._k = 6
+
+    def a(self):
+        a_func()
+
+
+class B:
+    def __init__(self):
+        self.a()
+        self._k = 6
 
     def a(self):
         a_func()
